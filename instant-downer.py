e2533d8e9b4c6ccb144c862b2f75c2ddd8e4d3bf
Zyney <65451125+Zyney@users.noreply.github.com>
2021-04-15 14:33:07 +0200

    Update instant-downer.py

--- instant-downer.py
+++ instant-downer.py
@@ -32,7 +32,7 @@ print(Fore.RED + """ /$$$$$$                       /$$                          
 | $$  \ $$| $$  | $$  | $$ /$$| $$  | $$| $$_____/  | $$ /$$        
 | $$$$$$$/|  $$$$$$/  |  $$$$/| $$  | $$|  $$$$$$$  |  $$$$/        
 |_______/  \______/    \___/  |__/  |__/ \_______/   \___/         """ + Style.RESET_ALL)
-ip = raw_input(color.BOLD + "Ip Adress: " + color.END)
+ip = raw_input("Ip Adress: ")
 port = input("Port: ")
 times = input("Time: ")
 timeout = time.time() + times
